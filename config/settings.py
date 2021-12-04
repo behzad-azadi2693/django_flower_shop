73c4ae8bd87b926758679c8cef9cd0d412826aaa
joe <48598137+joeliberal@users.noreply.github.com>
2021-12-04 13:37:17 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,7 +80,8 @@ if DEBUG:
     ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1','*']
 
     DATABASES = {
-        'default': {
+        'defaults';
+        'postgres': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
@@ -89,7 +90,8 @@ else:
     ALLOWED_HOSTS = ['flowershop.com', 'www.flowershop.com']
 
     DATABASES = {
-        'default': {
+        'defaults';
+        'postgres': {
             'ENGINE': 'django.db.backends.postgres',
             'NAME': 'flower',
             'USER':'admin',
