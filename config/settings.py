52a5a6d48baf9376e686c702bb21cd8a0a8ce9ee
joe <48598137+joeliberal@users.noreply.github.com>
2021-12-04 18:17:19 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local
+    'accounts.apps.AccountsConfig',
+    'api.apps.AccountsConfig',
+    'product.apps.ProductConfig',
 
     'django.contrib.sitemaps',
 
